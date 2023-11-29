562d399ae9398a6940280c32bd9a11aa729ede93
Melih Melik SÖNMEZ <5006654+themelic@users.noreply.github.com>
2023-11-29 15:27:31 +0300

    Update daily_tasks.sh
    usage report must be made after clear command to see clearly

--- linux/daily_tasks.sh
+++ linux/daily_tasks.sh
@@ -27,7 +27,7 @@ case "${ACTION}" in
   usage)
 
 ## System Load and Resource Usage Monitoring
-
+clear
 echo "System Load: $(uptime)"
 echo "Free Memory: $(free -h | grep Mem | awk '{print $4}')"
 echo "Free Disk Space: $(df -h / | grep / | awk '{print $4}')"
